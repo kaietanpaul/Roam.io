ec579841473be938a03910e42acbe4cb92a7da58
kaietanpaul <kaietanpaul@gmail.com>
2023-03-15 19:33:11 +0100

    show 3 forecast for city

--- weather/utils.py
+++ weather/utils.py
@@ -3,7 +3,10 @@ from django.conf import settings
 
 
 def fetch_weather_data(city_name):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={settings.OPENWEATHERMAP_API_KEY}&units=metric'
+    url = f'https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={settings.OPENWEATHERMAP_API_KEY}&units=metric'
     response = requests.get(url)
     data = response.json()
+
+    data['list'] = data['list'][:3]
+
     return data
--- weather/views.py
+++ weather/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from .utils import fetch_weather_data
+from datetime import datetime, timedelta
 
 
 def weather(request):
-    city_name = 'London'
+    city_name = 'Zielona Góra'
     weather_data = fetch_weather_data(city_name)
 
+    city = weather_data['city']['name']
+    forecast_list = []
+
+    for index, forecast in enumerate(weather_data['list']):
+        current_time = datetime.now()
+        hour = (current_time + timedelta(hours=index)).strftime('%H:%M')
+        forecast_list.append({
+            'hour': hour,
+            'temperature': forecast['main']['temp'],
+            'humidity': forecast['main']['humidity'],
+            'pressure': forecast['main']['pressure'],
+        })
+
     context = {
-        'city': weather_data['name'],
-        'temperature': weather_data['main']['temp'],
-        'description': weather_data['weather'][0]['description'],
+        'city': city,
+        'forecast_list': forecast_list,
     }
 
     return render(request, 'weather/weather.html', context)
