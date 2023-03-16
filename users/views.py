a88779480b8e1b74a626a43f6c9e72cb79a56822
kaietanpaul <kaietanpaul@gmail.com>
2023-03-16 17:55:27 +0100

    redirect to login

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('login')
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
