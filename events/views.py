5284450473d16596972984b4ae546a102ac29c98
kaietanpaul <kaietanpaul@gmail.com>
2023-03-21 14:32:19 +0100

    search for events when not logged in

--- events/views.py
+++ events/views.py
@@ -5,29 +5,39 @@ from cities.models import City
 
 
 def events(request):
-    user = request.user
-    favorite_cities = user.favorite_cities.all()
+    city_name = request.GET.get('city', None)
 
-    all_events = {}
-    for city in favorite_cities:
-        city_events = search_events(city.name, limit=3)
-        all_events[city] = city_events
+    if request.user.is_authenticated:
+        user = request.user
+        favorite_cities = user.favorite_cities.all()
 
-    if request.method == 'POST' and request.user.is_authenticated:
-        action = request.POST.get('action')
-        if action == 'edit':
-            city_id = request.POST.get('city_id')
-            new_city_name = request.POST.get('new_city_name')
-            if city_id and new_city_name:
-                city = City.objects.get(id=city_id)
-                city.name = new_city_name
-                city.save()
-        elif action == 'delete':
-            city_id = request.POST.get('city_id')
-            if city_id:
-                city = City.objects.get(id=city_id)
-                request.user.favorite_cities.remove(city)
-                request.user.save()
-        return redirect('events')
+        all_events = {}
+        for city in favorite_cities:
+            city_events = search_events(city.name, limit=3)
+            all_events[city] = city_events
 
-    return render(request, 'events/events.html', {'events': all_events})
+        if request.method == 'POST':
+            action = request.POST.get('action')
+            if action == 'edit':
+                city_id = request.POST.get('city_id')
+                new_city_name = request.POST.get('new_city_name')
+                if city_id and new_city_name:
+                    city = City.objects.get(id=city_id)
+                    city.name = new_city_name
+                    city.save()
+            elif action == 'delete':
+                city_id = request.POST.get('city_id')
+                if city_id:
+                    city = City.objects.get(id=city_id)
+                    request.user.favorite_cities.remove(city)
+                    request.user.save()
+            return redirect('events')
+
+        return render(request, 'events/events.html', {'events': all_events})
+
+    else:
+        if city_name:
+            city_events = search_events(city_name, limit=3)
+        else:
+            city_events = []
+        return render(request, 'events/events.html', {'city_events': city_events})
